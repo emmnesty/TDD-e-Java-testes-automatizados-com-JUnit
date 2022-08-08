eb96a50f95ac37ee0ceccae91a8ab98f7367b6e2
emmnesty <elyjhonatan@hotmail.com>
2022-08-08 11:16:00 -0300

    Organizando o código de teste, @BeforeEach, @AfterEach

--- src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -1,6 +1,7 @@
 package br.com.alura.tdd.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -14,24 +15,24 @@ class BonusServiceTest {
 	@Test
 	void bonusDeveriaSerZeroParaFuncionarioComSalarioAlto() {
 		BonusService service = new BonusService();
-		BigDecimal bonus = service.calcularBonus(new Funcionario("Jhonatan", LocalDate.now(), new BigDecimal("25000")));
-		
-		assertEquals(new BigDecimal("0.00"), bonus);
+		assertThrows(IllegalArgumentException.class,
+				() -> service.calcularBonus(new Funcionario("Jhonatan", LocalDate.now(), new BigDecimal("25000"))));
+
 	}
-	
+
 	@Test
 	void bonusDeveriaSer10PorCentoDoSalario() {
 		BonusService service = new BonusService();
 		BigDecimal bonus = service.calcularBonus(new Funcionario("Jhonatan", LocalDate.now(), new BigDecimal("2500")));
-		
+
 		assertEquals(new BigDecimal("250.00"), bonus);
 	}
-	
+
 	@Test
 	void bonusParaSalarioDeExatamente10Mil() {
 		BonusService service = new BonusService();
 		BigDecimal bonus = service.calcularBonus(new Funcionario("Jhonatan", LocalDate.now(), new BigDecimal("10000")));
-		
+
 		assertEquals(new BigDecimal("1000.00"), bonus);
 	}
 
